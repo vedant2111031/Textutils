02d0bdef3c14dd926c615c81cbcaa05088797bf8
vedant2111031 <vedant2111031@akgec.ac.in>
2023-11-02 00:04:33 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,46 @@
-import { type } from '@testing-library/user-event/dist/type';
+
 import './App.css';
-import Alert from './components/Alert';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import React, {useState} from 'react';
+
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar'
 
 
-function App() {
-  const [mode,setMode] = useState('light');
-  const [alert, setAlert] = useState(null);
-
-  const showAlert =(message, type)=>{
-    setAlert({
-      msg: message,
-      type : type
-     })
-     setTimeout(() => {
-      setAlert(null);
-     }, 1000);
+export default class App extends Component {
+  apiKey="d9c6c8df31ad4a1a85bf258385c39088"
+  state={
+    progress:0
   }
-  const toggleMode = ()=>{
-      if (mode === 'light'){
-        setMode('dark');
-        document.body.style.backgroundColor ='grey';
-        showAlert("Dark mode enable", "success");
-      }
-      else{
-        setMode('light');
-        document.body.style.backgroundColor ='white';
-        showAlert("Light mode enabled","success");
-      }
+  setProgress=(progress)=>{
+    this.setState({progress:progress})
   }
-  return (
-    <>
-    
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert}/>
-      <div className="container my-3">
-       <TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert}/> 
+  render() {
+    return (
+      <div>
+        <Router>
+          <NavBar />
+          <LoadingBar
+        color='#f11946'
+        progress={this.state.progress}
+        
+      />
+          <Routes>
+            <Route path='/' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='general' pageSize={18} country='in' category='general' />}></Route>
+            <Route path='/business' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='business' pageSize={18} country='in' category='business' />}></Route>
+            <Route path='/entertainment' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='entertainment' pageSize={18} country='in' category='entertainment' />}></Route>
+            <Route path='/general' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='general' pageSize={18} country='in' category='general' />}></Route>
+            <Route path='/health' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='health' pageSize={18} country='in' category='health' />}></Route>
+            <Route path='/science' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='science' pageSize={18} country='in' category='science' />}></Route>
+            <Route path='/sports' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='sports' pageSize={18} country='in' category='sports' />}></Route>
+            <Route path='/technology' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='technology' pageSize={18} country='in' category='technology' />}></Route>
+            <Route path='/US' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='US' pageSize={18} country='us' category='general' />}></Route>
+            <Route path='/Russia' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='Russia' pageSize={18} country='rs' category='general' />}></Route>
+            <Route path='/Australia' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='Australia' pageSize={18} country='au' category='general' />}></Route>
+          </Routes>
+        </Router> 
       </div>
-      
-    </>
-  );
+    )
+  }
 }
-
-export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,57 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react'
 
 
-export default function Navbar(props) {
-  return (
-    <div>
-      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">{props.title}</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="/">Home</a>
-              </li>
-            </ul>
-            <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-                <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-                <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+export class NavBar extends Component {
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">Top-Headlines</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/general">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/business">Business</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/entertainment">Entertainment</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/health">Health</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/science">Science</a></li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/sports">Sports</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/technology">Technology</a>
+                </li>
+                <li className="nav-item dropdown d-flex"style={{marginRight:"10px"}}>
+                  <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Country
+                  </a>
+                  <ul className="dropdown-menu" aria-labelledby="navbarDropdown" >
+                    <li><a className="dropdown-item" href="/US">US</a></li>
+                    <li><a className="dropdown-item" href="/Russia">Russia</a></li>
+                    <li><a className="dropdown-item" href="/Australia">Australia</a></li>
+                  </ul>
+                </li>
+              </ul>
+              
             </div>
           </div>
-        </div>
-      </nav>
-    </div>
-  )
+        </nav>
+      </div>
+    )
+  }
 }
+
+export default NavBar
